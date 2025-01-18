94c2fa44a44c7b5dfc89c08b9cf252dfc4316459
RZuchniak <robert.zuchniak@hotmail.com>
2025-01-18 13:41:02 -0500

    fix: separate login page, file formatting

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	let { form }: { form: ActionData } = $props();
+</script>
+
+<div class="flex min-h-screen flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
+	<div class="sm:mx-auto sm:w-full sm:max-w-md">
+		<div class="flex justify-center">
+			<svg version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" width="64px" height="64px" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <style type="text/css"> .st0{fill:#000000;} </style> <g> <rect x="115.774" y="335.487" class="st0" width="194.387" height="18.588"></rect> <rect x="115.774" y="260.208" class="st0" width="194.387" height="18.527"></rect> <rect x="115.774" y="184.862" class="st0" width="194.387" height="18.588"></rect> <rect x="218.582" y="109.576" class="st0" width="91.58" height="18.588"></rect> <path class="st0" d="M385.112,396.188V39.614c0-2.294-0.197-4.603-0.592-6.768C381.3,14.19,365.006,0,345.438,0H184.686 c-11.561,0-22.598,4.603-30.741,12.747L53.637,112.986c-8.151,8.22-12.747,19.249-12.747,30.818v252.384 c0,21.802,17.806,39.607,39.683,39.607h264.864C367.308,435.795,385.112,417.99,385.112,396.188z M170.634,27.529v89.074 c0,9.662-3.745,13.399-13.339,13.399H68.222L170.634,27.529z M63.163,396.188V149.775h106.02c3.486,0,6.768-0.85,9.655-2.362 c4.079-2.036,7.361-5.324,9.328-9.328c1.519-2.894,2.302-6.115,2.302-9.526V22.272h154.97c7.156,0,13.331,4.33,15.959,10.574 c0.92,2.104,1.376,4.337,1.376,6.768v356.574c0,9.518-7.748,17.342-17.335,17.342H80.574 C70.98,413.53,63.163,405.706,63.163,396.188z"></path> <path class="st0" d="M431.488,76.205h-26.732l1.375,22.272h25.356c9.594,0,17.349,7.748,17.349,17.342v356.573 c0,9.519-7.755,17.342-17.349,17.342H166.562c-7.163,0-13.339-4.406-15.968-10.581c-0.85-2.097-1.374-4.33-1.374-6.761V456.89 h-22.272v15.503c0,2.294,0.198,4.589,0.593,6.761c3.22,18.588,19.515,32.846,39.022,32.846h264.926 c21.877,0,39.622-17.805,39.622-39.607V115.82C471.11,93.943,453.365,76.205,431.488,76.205z"></path> </g> </g></svg>
+		</div>
+		<h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Create your account</h2>
+		<p class="mt-2 text-center text-sm text-gray-600">
+			Or
+			<a href="/login" class="font-medium text-blue-600 hover:text-blue-500">
+				Log in to an existing account
+			</a>
+		</p>
+	</div>
+
+	<div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+		<div class="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+			<form class="space-y-6" method="post" action="?/login" use:enhance>
+				<div>
+					<label for="username" class="block text-sm font-medium text-gray-700"> Username </label>
+					<div class="mt-1">
+						<input
+							name="username"
+							required
+							class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+						/>
+					</div>
+				</div>
+
+				<div>
+					<label for="password" class="block text-sm font-medium text-gray-700"> Password </label>
+					<div class="mt-1">
+						<input
+							id="password"
+							name="password"
+							type="password"
+							required
+							class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+						/>
+					</div>
+				</div>
+				<button
+					formaction="?/register"
+					type="submit"
+					class="flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50"
+				>
+					Register
+				</button>
+			</form>
+			<p style="color: red">{form?.message ?? ''}</p>
+		</div>
+	</div>
+</div>
