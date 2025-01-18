afa93b9b38a67fbe4ec2d05141ed249e2782dd05
RZuchniak <robert.zuchniak@hotmail.com>
2025-01-18 11:32:06 -0500

    feat: style login page

--- src/routes/demo/lucia/login/+page.svelte
+++ src/routes/demo/lucia/login/+page.svelte
@@ -5,17 +5,59 @@
 	let { form }: { form: ActionData } = $props();
 </script>
 
-<h1>Login/Register</h1>
-<form method="post" action="?/login" use:enhance>
-	<label>
-		Username
-		<input name="username" />
-	</label>
-	<label>
-		Password
-		<input type="password" name="password" />
-	</label>
-	<button>Login</button>
-	<button formaction="?/register">Register</button>
-</form>
-<p style="color: red">{form?.message ?? ''}</p>
+<div class="flex min-h-screen flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
+	<div class="sm:mx-auto sm:w-full sm:max-w-md">
+		<div class="flex justify-center">
+			<img src="" alt="Logo" class="h-8 w-auto" />
+		</div>
+		<h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+			Sign in or register your account
+		</h2>
+	</div>
+
+	<div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+		<div class="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+			<form class="space-y-6" method="post" action="?/login" use:enhance>
+				<div>
+					<label for="username" class="block text-sm font-medium text-gray-700"> Username </label>
+					<div class="mt-1">
+						<input
+							name="username"
+							required
+							class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+						/>
+					</div>
+				</div>
+
+				<div>
+					<label for="password" class="block text-sm font-medium text-gray-700"> Password </label>
+					<div class="mt-1">
+						<input
+							id="password"
+							name="password"
+							type="password"
+							required
+							class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+						/>
+					</div>
+				</div>
+				<div class="flex gap-4">
+					<button
+						type="submit"
+						class="flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50"
+					>
+						Sign in
+					</button>
+					<button
+						formaction="?/register"
+						type="submit"
+						class="flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50"
+					>
+						Register
+					</button>
+				</div>
+			</form>
+			<p style="color: red">{form?.message ?? ''}</p>
+		</div>
+	</div>
+</div>
