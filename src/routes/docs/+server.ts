e4f1068c2c4b95f25cde6d3581ecf582e0f0f181
matteopolak <matteopolak@hotmail.com>
2025-01-19 01:47:21 -0500

    feat: add asyncapi docs

---
+++ src/routes/docs/+server.ts
@@ -0,0 +1,34 @@
+export async function GET(event) {
+	if (!event.locals.user) {
+		return error(403);
+	}
+
+	const form = await event.request.formData();
+	const image = form.get('image') as string;
+
+	// groq
+	const response = await groq.chat.completions.create({
+		messages: [
+			{
+				role: 'user',
+				content: [
+					{
+						type: 'text',
+						text: 'Create alt text for this image. It should be brief and at most 100 characters long.'
+					},
+					{
+						type: 'image_url',
+						image_url: {
+							url: image
+						}
+					}
+				]
+			}
+		],
+		model: 'llama-3.2-11b-vision-preview'
+	});
+
+	return json({
+		altText: response.choices[0].message.content!
+	});
+}
